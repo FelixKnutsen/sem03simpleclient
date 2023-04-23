814d8a8cb13803fc62b47e5ac17c121503a2d6b8
FelixKnutsen <felixfk@uia.no>
2023-04-23 20:27:34 +0200

    gjorde punk om cryot

--- tcp_client.go
+++ tcp_client.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
-	"github.com/FelixKnutsen/is105sem03/mycrypt"
-	)
 
-	func main() {
+	"github.com/FelixKnutsen/is105sem03/mycrypt"
+)
+
+func main() {
 	conn, err := net.Dial("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
-
- 	_, err = conn.Write([]byte(os.Args[1]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
-
 
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 	_, err = conn.Write([]byte(string(kryptertMelding)))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Printf("reply from proxy: %s", string(dekryptertMelding))
 }
